9920e156b7e01845302c2674c33cabe34e5f2c28
George Damotsov <george.damotsov@gmail.com>
2022-02-07 21:06:39 +0300

    daemon

---
+++ thread2.py
@@ -0,0 +1,16 @@
+import threading
+import time
+
+#потоки ,демоны
+
+
+def get_data(data):
+    for _ in range(5):
+        print(f'[{threading.currentThread().name}] - {data}')
+        time.sleep(1)
+ 
+
+thr = threading.Thread(target=get_data, args=(str(time.time()),), daemon=True)
+thr.start()
+time.sleep(1)
+print('Finish')
